782ed8e07fd645fbd420ebb9d613e6e446a0481e
SympactDev <47295195+SympactDev@users.noreply.github.com>
2021-03-11 09:14:32 +0100

    Update PyFileCreator.py

--- PyFileCreator.py
+++ PyFileCreator.py
@@ -1,21 +1,32 @@
 import time
+import os
+import sys
 
+# Variables 
 reason = 'Please type the file you want to create. (Including extension)'
 done = 'Done! have fun!'
 after = 'This script will be closed automatically after 30 seconds.'
-br = ''
+blankracket = ''
 
 
-print(reason)
-print(br)
+def naarconsole(zin):
+  print("--------------------------------")
+  print(zin)
+  print("--------------------------------")
+
+    
+
+naarconsole(reason)
+naarconsole(blankracket)
 time.sleep(1)
 
 input = input()
 f = open(input, "w")
 time.sleep(1)
 
-print(br)
-print(done)
-print(br)
-print(after)
+naarconsole(blankracket)
+naarconsole(done)
+naarconsole(blankracket)
+naarconsole(after)
 time.sleep(30)
+sys.exit()
